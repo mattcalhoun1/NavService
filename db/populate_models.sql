28baae6c374ffdd0789607d43745aac8317fa392
Matt <mattcalhoun1@gmail.com>
2023-08-24 09:02:33 -0400

    add lights model

--- db/populate_models.sql
+++ db/populate_models.sql
@@ -9,13 +9,34 @@ VALUES (
     'basement',
     'object_detection',
     'tflite',
-    '{"mapping_file":"/home/matt/projects/Robotics/NavigationModels/FinishedModels/basement.txt"}',
+    '{"mapping_file":"/home/matt/projects/Robotics/NavigationModels/FinishedModels/basement.txt","object_type":"basic","resolution_w":640,"resolution_h":640}',
     '/home/matt/projects/Robotics/NavigationModels/FinishedModels/basement.tflite'
 ) 
 ON CONFLICT (model_id, model_type, model_format) 
-DO NOTHING
+DO UPDATE set
+    additional_params = EXCLUDED.additional_params,
+    file_location = EXCLUDED.file_location
 ;
 
+INSERT INTO nav.models (
+    model_id, 
+    model_type, -- ex : object_detection
+    model_format, -- ex: tflite
+    additional_params, -- ex: obj number / name mapping, etc
+    file_location
+)
+VALUES (
+    'lights',
+    'object_detection',
+    'tflite',
+    '{"mapping_file":"/home/matt/projects/Robotics/NavigationModels/FinishedModels/lights.txt","object_type":"emitter","resolution_w":640,"resolution_h":640}',
+    '/home/matt/projects/Robotics/NavigationModels/FinishedModels/lights_640.tflite'
+) 
+ON CONFLICT (model_id, model_type, model_format) 
+DO UPDATE set
+    additional_params = EXCLUDED.additional_params,
+    file_location = EXCLUDED.file_location
+;
 
 
 
