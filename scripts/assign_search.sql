96174bed88f58c069d685277560c2486a571fc46
Matt <mattcalhoun1@gmail.com>
2023-10-17 13:49:57 -0400

    add scripts

---
+++ scripts/assign_search.sql
@@ -0,0 +1,10 @@
+INSERT INTO nav.assignments 
+(
+    vehicle_id,
+    assignment
+)
+VALUES
+(
+    'MecCar',
+    '{"steps":[{"command":"SEARCH", "params": {"type":"basic", "objects":["gazing_ball"]}}], "map_id":"basement", "vehicle_id":"MecCar"}'
+);
