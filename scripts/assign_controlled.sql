25349dd450f238fdcc4e07bbd247bd32696445bf
Matt <mattcalhoun1@gmail.com>
2023-10-23 15:58:11 -0400

    add script for testing controlled

---
+++ scripts/assign_controlled.sql
@@ -0,0 +1,10 @@
+INSERT INTO nav.assignments 
+(
+    vehicle_id,
+    assignment
+)
+VALUES
+(
+    'MecCar',
+    '{"steps":[{"command":"CONTROLLED", "params": {"type":"basic"}}], "map_id":"basement", "vehicle_id":"MecCar"}'
+);
