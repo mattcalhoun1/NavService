e20798712f5ec9dc63a975a7deabc995bf06b057
Matt <mattcalhoun1@gmail.com>
2023-11-10 09:46:47 -0500

    add new basement maps

--- db/populate_maps.sql
+++ db/populate_maps.sql
@@ -21,3 +21,51 @@ set
     file_location = EXCLUDED.file_location,
     is_active = EXCLUDED.is_active
 ;
+
+INSERT INTO nav.maps (
+    map_id, 
+    map_desc,
+    map_content,
+    file_location,
+    is_active
+)
+VALUES (
+    'basement_v2',
+    'Basement V2 - Pole 0,0 lights and objects',
+    '{}',
+    '/home/matt/projects/services/NavService/maps/basement_v2.json',
+    true
+
+) 
+ON CONFLICT (map_id) 
+DO UPDATE 
+set 
+    map_content = EXCLUDED.map_content,
+    map_desc = EXCLUDED.map_desc,
+    file_location = EXCLUDED.file_location,
+    is_active = EXCLUDED.is_active
+;
+
+INSERT INTO nav.maps (
+    map_id, 
+    map_desc,
+    map_content,
+    file_location,
+    is_active
+)
+VALUES (
+    'basement_lights',
+    'Basement V2 - Pole 0,0 lights only',
+    '{}',
+    '/home/matt/projects/services/NavService/maps/basement_lights.json',
+    true
+
+) 
+ON CONFLICT (map_id) 
+DO UPDATE 
+set 
+    map_content = EXCLUDED.map_content,
+    map_desc = EXCLUDED.map_desc,
+    file_location = EXCLUDED.file_location,
+    is_active = EXCLUDED.is_active
+;
